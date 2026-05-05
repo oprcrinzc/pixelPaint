9b5328a6d1e275f6819ed5e676b8fddc27c4e3bf
oprc <chisainux@gmail.com>
2026-05-05 10:13:19 +0700

    InputText completed

--- component/inputText.go
+++ component/inputText.go
@@ -2,6 +2,7 @@ package component
 
 import (
 	"image/color"
+	"time"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -19,14 +20,18 @@ type InputText struct {
 	origin      rl.Vector2
 	cursorPos   int
 	fontSize    int32
+	blinkTime   int64
+	onBlink     int64
+	offBlink    int64
+	nextBlink   int64
+	lastKeyNum  int32
 }
 
 // -------------------------------------------------------------------------
 
 // New create InputText object
-func (it *InputText) New(w, h int32, orin rl.Vector2) *InputText {
+func (it *InputText) New(w int32, orin rl.Vector2) *InputText {
 	it.width = w
-	it.height = h
 	it.bgColor = color.RGBA{234, 135, 234, 255}
 	it.textColor = color.RGBA{65, 36, 65, 255}
 	it.cursorColor = color.RGBA{72, 34, 255, 255}
@@ -35,6 +40,12 @@ func (it *InputText) New(w, h int32, orin rl.Vector2) *InputText {
 	it.origin = orin
 	it.cursorPos = 0
 	it.fontSize = 50
+	it.height = it.fontSize
+	it.blinkTime = 900
+	it.onBlink = 900
+	it.offBlink = 900
+	it.nextBlink = time.Now().UnixMilli() + int64(it.blinkTime)
+	it.lastKeyNum = 0
 	return it
 }
 
@@ -54,9 +65,9 @@ func (it *InputText) IsFocused() bool {
 // -------------------------------------------------------------------------
 // Set
 
-// Focus set focused to true
-func (it *InputText) Focus() *InputText {
-	it.focused = true
+// SetFocus set focused to true
+func (it *InputText) SetFocus(f bool) *InputText {
+	it.focused = f
 	return it
 }
 
@@ -64,9 +75,62 @@ func (it *InputText) Focus() *InputText {
 
 // Render render InoutText
 func (it *InputText) Render() {
+	pdL := 10
+	pdT := 5
+
+	mousePos := rl.GetMousePosition()
+
 	rl.DrawRectangle(int32(it.origin.X), int32(it.origin.Y), it.width, it.height, it.bgColor)
+	rl.DrawText(it.value, int32(it.origin.X)+int32(pdL), int32(it.origin.Y)+int32(pdT), it.fontSize, it.textColor)
+
+	if (mousePos.X >= it.origin.X && mousePos.X <= it.origin.X+float32(it.width)) &&
+		(mousePos.Y >= it.origin.Y && mousePos.Y <= it.origin.Y+float32(it.height)) {
+		rl.SetMouseCursor(rl.MouseCursorIBeam)
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			it.SetFocus(true)
+		}
+	} else {
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			it.SetFocus(false)
+		}
+
+		rl.SetMouseCursor(rl.MouseCursorDefault)
+	}
+
 	if it.focused {
-		rl.DrawRectangle(int32(20+it.cursorPos+10/2), 10, int32(0.25*float64(it.fontSize)), it.fontSize, it.cursorColor)
+
+		key := rl.GetKeyPressed()
+
+		if (key >= 48 && key <= 57) || rl.IsKeyPressedRepeat(it.lastKeyNum) {
+			it.nextBlink = it.nextBlink + 200
+			if rl.IsKeyPressedRepeat(it.lastKeyNum) {
+				it.value += string(byte(it.lastKeyNum))
+			} else {
+				it.value += string(byte(key))
+				it.lastKeyNum = key
+			}
+		}
+
+		lenValue := len(it.value)
+
+		if (key == 259 || rl.IsKeyPressedRepeat(259)) && lenValue > 0 {
+			it.nextBlink += 200
+			it.value = it.value[:lenValue-1]
+		}
+
+		it.cursorPos = lenValue
+		tsize := rl.MeasureText(it.value, it.fontSize)
+
+		if time.Now().UnixMilli() <= it.nextBlink-it.offBlink {
+			rl.DrawRectangle(int32(it.origin.X)+int32(pdL+int(tsize)+10),
+				int32(it.origin.Y)+int32(pdT),
+				int32(0.1*float64(it.fontSize)),
+				it.fontSize-int32(2*(pdT)), it.cursorColor)
+		}
+		if time.Now().UnixMilli() > it.nextBlink {
+			it.nextBlink = time.Now().UnixMilli() + int64(it.blinkTime) + it.onBlink
+		}
+
 	}
 }
 
--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"pixelpaint/component"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
 	rl.SetConfigFlags(rl.FlagWindowResizable)
 	rl.InitWindow(600, 500, "Pixel Paint")
 	defer rl.CloseWindow()
 
+	rl.SetExitKey(rl.KeyNull)
 	rl.SetTargetFPS(60)
+
+	it1 := component.InputText{}
+	it1.New(300, rl.NewVector2(300, 50))
+
+	// fmt.Println(string([]byte{'A', 'B'}))
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Width", 100, 50, 50, rl.Black)
 		rl.DrawText("Height", 100, 120, 50, rl.Black)
+		it1.Render()
 		rl.EndDrawing()
 	}
 }
